a9821ba29853486c1285f23505f94b6a2754999f
AnnushkaStark <130693211+AnnushkaStark@users.noreply.github.com>
2024-09-06 01:31:40 +0300

    added message router

--- src/api/v1/router.py
+++ src/api/v1/router.py
@@ -2,7 +2,11 @@ from fastapi import APIRouter
 
 from api.v1.endpoints.auth import router as auth_router
 from api.v1.endpoints.chat import router as chat_router
+from api.v1.endpoints.message import router as message_router
 
 api_router = APIRouter(prefix="/api/v1")
 api_router.include_router(auth_router, prefix="/auth", tags=["Auth"])
 api_router.include_router(chat_router, prefix="/chat", tags=["Chat"])
+api_router.include_router(
+    message_router, prefix="/messages", tags=["Messages"]
+)
